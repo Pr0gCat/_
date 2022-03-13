f85db77d5be77aca3b7f78cf27952ce2220c71d9
ProgCat <progcat@protonmail.com>
2022-03-14 00:49:06 +0800

    added node version check for parallel parsing

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import semvar from 'semver';
+// Do a node version check
+if (!semvar.satisfies(process.version, '>=12.0.0')) {
+    console.error(
+        'Wow, you are using really old node version. \
+        You should upgrade to 12.0.0 or higher in order to use this compiler.'
+    );
+    process.exit(1);
+}
